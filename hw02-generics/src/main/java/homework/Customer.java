0aa600d830d0077102c55429227928188b906fe2
sergey <for_s88@mail.ru>
2022-06-08 00:19:57 +0700

    hw02-generics

--- hw02-generics/src/main/java/homework/Customer.java
+++ hw02-generics/src/main/java/homework/Customer.java
@@ -1,11 +1,9 @@
 package homework;
 
-public class Customer {
+public class Customer implements Cloneable {
     private final long id;
     private String name;
     private long scores;
-
-    //todo: 1. в этом классе надо исправить ошибки
 
     public Customer(long id, String name, long scores) {
         this.id = id;
@@ -49,16 +47,20 @@ public class Customer {
 
         Customer customer = (Customer) o;
 
-        if (id != customer.id) return false;
-        if (scores != customer.scores) return false;
-        return name != null ? name.equals(customer.name) : customer.name == null;
+        return id == customer.id;
     }
 
     @Override
     public int hashCode() {
-        int result = (int) (id ^ (id >>> 32));
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        result = 31 * result + (int) (scores ^ (scores >>> 32));
-        return result;
+        return (int) (id ^ (id >>> 32));
+    }
+
+    @Override
+    public Customer clone() {
+        try {
+            return (Customer) super.clone();
+        } catch (CloneNotSupportedException e) {
+            return new Customer(this.id, this.name, this.scores);
+        }
     }
 }
--- hw02-generics/src/main/java/homework/CustomerService.java
+++ hw02-generics/src/main/java/homework/CustomerService.java
@@ -1,23 +1,28 @@
 package homework;
 
 
-import java.util.Map;
+import java.util.*;
 
 public class CustomerService {
 
-    //todo: 3. надо реализовать методы этого класса
-    //важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
+    NavigableMap<Customer, String> services = new TreeMap<>(Comparator.comparingLong(Customer::getScores));
+
+    private static Map.Entry<Customer, String> cloneEntry(Map.Entry<Customer, String> entry) {
+        return Optional
+                .ofNullable(entry)
+                .map(source -> Map.entry(source.getKey().clone(), source.getValue()))
+                .orElse(null);
+    }
 
     public Map.Entry<Customer, String> getSmallest() {
-        //Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
-        return null; // это "заглушка, чтобы скомилировать"
+        return cloneEntry(services.firstEntry());
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
-        return null; // это "заглушка, чтобы скомилировать"
+        return cloneEntry(services.higherEntry(customer));
     }
 
     public void add(Customer customer, String data) {
-
+        services.put(customer, data);
     }
 }
